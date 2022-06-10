c932dc8fc5ebb8bb5d013233092eca93c478fa95
Ankit Singh <er.ankit.cs@gmail.com>
2022-06-11 01:45:29 +1000

    log service server

---
+++ distributedapp/go.mod
@@ -0,0 +1,3 @@
+module github.com/erankitcs/golang_learning/distributedapp
+
+go 1.17
---
+++ distributedapp/log/server.go
@@ -0,0 +1,40 @@
+package log
+
+import (
+	"io/ioutil"
+	stlog "log"
+	"net/http"
+	"os"
+)
+
+var log *stlog.Logger
+
+type fileLog string
+
+func (fl fileLog) Write(data []byte) (int, error) {
+	f, err := os.OpenFile(string(fl), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	return f.Write(data)
+}
+
+func Run(destination string) {
+	log = stlog.New(fileLog(destination), "", stlog.LstdFlags)
+}
+
+func RegisterHandler() {
+	http.HandleFunc("/log", func(rw http.ResponseWriter, r *http.Request) {
+		msg, err := ioutil.ReadAll(r.Body)
+		if err != nil && len(msg) == 0 {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		write(string(msg))
+	})
+}
+
+func write(msg string) {
+	log.Printf("/%v\n", msg)
+}
